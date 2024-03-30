1bff8fe6673257ae94fb29ffa0e51eff763a93e0
Vaibhav Gaur <64890655+VaibhavGaur05@users.noreply.github.com>
2024-03-30 16:42:49 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+// "33" changes to 33
+// "33abc" changes to NaN
+// true changes to 1 false to 0
+//conversion of empty string is false anything else on it si true 
