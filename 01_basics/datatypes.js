44842273808f1d244b3d3902eac814cf86d81079
Vaibhav Gaur <64890655+VaibhavGaur05@users.noreply.github.com>
2024-03-30 15:39:23 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //this treats all js code as newer version
+
+// alert("hello") we are using node js not browser
+
+let name = vaibhav
+let age = 23
+let isLoggedIn = true  
+
+//boolean = true/flase
+//string = ""
+//null is also a standalone value or type
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
