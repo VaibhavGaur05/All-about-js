bcd9571dd6cc29c5d932c24215d8033ab3d880cb
Vaibhav Gaur <64890655+VaibhavGaur05@users.noreply.github.com>
2024-03-31 17:35:53 +0000

    small changes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,17 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 
 // "33" changes to 33
 // "33abc" changes to NaN
 // true changes to 1 false to 0
 //conversion of empty string is false anything else on it si true 
+
+
+// OPERATIONS | OPERATIONS
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
